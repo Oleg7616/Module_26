037943829894d511b5e5a10d91716372bcec4522
Oleg Leontjev <oleg.leontjev@mail.ru>
2021-07-28 18:10:14 +0300

    init

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -28,6 +28,10 @@ class ComposerAutoloaderInit49d73347655953ee54450fb3294a4ccd
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit49d73347655953ee54450fb3294a4ccd', 'loadClassLoader'));
 
+        $includePaths = require __DIR__ . '/include_paths.php';
+        $includePaths[] = get_include_path();
+        set_include_path(implode(PATH_SEPARATOR, $includePaths));
+
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require __DIR__ . '/autoload_static.php';
