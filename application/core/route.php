c73129646c47df2a1ef8851e2ab19e5f2f1c1ef0
Oleg Leontjev <oleg.leontjev@mail.ru>
2021-10-02 10:38:13 +0300

    init

--- application/core/route.php
+++ application/core/route.php
@@ -13,30 +13,28 @@ class Route {
             $controller_name = $routes;
         }
 
-        $model_name = 'model_' .$controller_name;
-        $controller_name = 'controller_' .$controller_name;
-        $action_name = 'action_' .$action_name;
+        $model_name = 'model_'.$controller_name;
+        $controller_name = 'controller_'.$controller_name;
+        $action_name = 'action_'.$action_name;
 
-        /*echo "Model: $model_name <br>";
-        echo "Controller: $controller_name <br>";
-        echo "Action: $action_name <br>";*/
-
-        $model_file = strtolower($model_name) . '.php';
-        $model_path = 'application/models/' .$model_file;
+        $model_file = strtolower($model_name).'.php';
+        $model_path = 'application/models/'.$model_file;
         
         if (file_exists($model_path)) {
-             include 'application/models/' .$model_file;
+             include 'application/models/'.$model_file;
          }
 
-        $controller_file = strtolower($controller_name) . '.php';
-        $controller_path = 'application/controllers/' .$controller_file;
+        $controller_file = strtolower($controller_name).'.php';
+        $controller_path = 'application/controllers/'.$controller_file;
         
         if (file_exists($controller_path)) {
-             include 'application/controllers/' .$controller_file;
+             include 'application/controllers/'.$controller_file;
          }
 
-         else {
-             Route::ErrorPage404();
+         else 
+         {
+            Route::ErrorPage404();
+            //echo 'Страница недоступна 404';
          }
 
          $controller = new $controller_name;
@@ -46,16 +44,20 @@ class Route {
             $controller->$action();
          }
 
-         else {
+         else 
+         {
             Route::ErrorPage404();
+            //echo 'Страница недоступна 404';
         }
     }
 
+
         function ErrorPage404() {
-        $host = 'http://' . $_SERVER['HTTP_HOST'];
-        header('HTTP/1.1 404 NOT FOUND');
+        $host = 'http://'.$_SERVER['HTTP_HOST'].'/';
+        header('HTTP/1.1 404 Not Found');
         header('Status: 404 Not Found');
-        header('location:'. $host .'404');
+        header('location:'.$host.'404');
         }
 
 }
+
